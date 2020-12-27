c63f9b04337510022b656f2343f41b04f32fd432
Ngyu-Phee Yen <yyuan@gmx.com>
2020-12-26 19:28:17 -0800

    Stored personal build command sequence.

---
+++ priv_build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd ~/Source/halide_build
+mkdir apps
+cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_DIR=/usr/lib64/cmake/llvm -DCMAKE_MODULE_PATH=~/.local/git/Halide/cmake/Modules ~/.local/git/Halide/
+make all -j`nproc`
+cmake --install . --prefix ./_Halide_install
+cd apps
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH=~/.local/git/Halide/cmake/Modules -DCMAKE_PREFIX_PATH=~/Source/halide_build/_Halide_install ~/.local/git/Halide/apps
