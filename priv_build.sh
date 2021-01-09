dae01760c350e23547000d4e53685c2b2c63d5b1
Ngyu-Phee Yen <yyuan@gmx.com>
2021-01-09 10:54:23 -0800

    Updated build command lines.

--- priv_build.sh
+++ priv_build.sh
@@ -6,4 +6,4 @@ cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_DIR=/usr/lib64/cmake/llvm -DCMAKE_MODULE
 make all -j`nproc`
 cmake --install . --prefix ./_Halide_install
 cd apps
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH=~/.local/git/Halide/cmake/Modules -DCMAKE_PREFIX_PATH=~/Source/halide_build/_Halide_install ~/.local/git/Halide/apps
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH=~/.local/git/Halide/cmake/Modules -DCMAKE_PREFIX_PATH=~/Source/halide_build/_Halide_install -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda ~/.local/git/Halide/apps
